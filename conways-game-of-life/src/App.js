09001c11b499ad4cdf207fea404cd9aa8e03a2f6
tevin davis <aod.td1105@gmail.com>
2020-09-28 19:50:31 -0400

    Added page to show instructions of game

--- conways-game-of-life/src/App.js
+++ conways-game-of-life/src/App.js
@@ -1,184 +1,33 @@
-import React, { useState, useCallback, useRef, useEffect } from "react";
+import React, { useState } from "react";
 import './App.css';
-import produce from "immer";
-
-
-const operations = [
-  [0, 1],
-  [0, -1],
-  [1, -1],
-  [-1, 1],
-  [1, 1],
-  [-1, -1],
-  [1, 0],
-  [-1, 0]
-];
+import Grid from './components/Grid';
 
 
 
 function App() {
-  const [gridSize, setGridSize] = useState({
-    numCols: 30,
-    numRows: 30
-  })
-
-  const generateEmptyGrid = () => {
-    const rows = [];
-    for (let i = 0; i < gridSize.numRows; i++) {
-      rows.push(Array.from(Array(gridSize.numCols), () => 0));
-    }
-  
-    return rows;
-  };
-  const [generation, setGeneration] = useState(-1)
-  const [speed, setSpeed] = useState(500)
-  
-
-  const [grid, setGrid] = useState(() => {
-    return generateEmptyGrid();
-  });
-
-  useEffect(() => {
-    setGeneration(generation + 1)
-  }, [grid])
-
-  const [running, setRunning] = useState(false);
-
-  const runningRef = useRef(running);
-  runningRef.current = running;
-
-
-
-    let runSimulation = useCallback(() => {
-      if (!runningRef.current) {
-        return;
-      }
-        setGrid(g => {
-          //  The rules for the game of life
-          return produce(g, gridCopy => {
-            for (let i = 0; i < gridSize.numRows; i++) {
-              for (let k = 0; k < gridSize.numCols; k++) {
-                let neighbors = 0;
-                operations.forEach(([x, y]) => {
-                  const newI = i + x;
-                  const newK = k + y;
-                  if (newI >= 0 && newI < gridSize.numRows && newK >= 0 && newK < gridSize.numCols) {
-                    neighbors += g[newI][newK];
-                  }
-                });
-    
-                if (neighbors < 2 || neighbors > 3) {
-                  gridCopy[i][k] = 0;
-                } else if (g[i][k] === 0 && neighbors === 3) {
-                  gridCopy[i][k] = 1;
-                }
-              }
-            }
-          });
-        });
-          setTimeout(runSimulation, speed);  
-    }, [gridSize, speed]);
+  const [page, setPage] = useState(true)
 
   return (
-    <div style={{display: 'flex', flexDirection: 'row-reverse', justifyContent: 'flex-end', margin: '20px'}}>
-      <div style={{display: 'flex', flexDirection: 'column'}}>
-        <button
-          className='button'
-          onClick={() => {
-            setRunning(!running);
-            if (!running) {
-              runningRef.current = true;
-              runSimulation();
-            }
-          }}
-        >
-          {running ? "stop" : "start"}
-        </button>
-
-        {/*randomly set cells on grid*/}
-        <button
-          className='button'
-          onClick={() => {
-            const rows = [];
-            for (let i = 0; i < gridSize.numRows; i++) {
-              rows.push(
-                Array.from(Array(gridSize.numCols), () => (Math.random() > 0.7 ? 1 : 0))
-              );
-            }
-
-            setGrid(rows);
-          }}
-        >
-          random
-        </button>
-
-        {/*clear all cells on grid*/}
-        <button
-          className='button'
-          onClick={() => {
-            setGrid(generateEmptyGrid());
-            setRunning(false)
-            setGeneration(-1)
-          }}
-        >
-          clear
-        </button>
-        {/* Grid size */}
-        <label className='label-style'>Grid Size:</label>
-        <div> 
-          <button className='small-button' onClick={() => setGridSize({numRows: 50, numCols: 50})}>Large</button>
-          <button className='small-button' onClick={() => setGridSize({numRows: 30, numCols: 30})}>Normal</button>
-          <button className='small-button' onClick={() => setGridSize({numRows: 15, numCols: 15})}>Small</button>
+    <>
+      {
+        page 
+        ?
+        <div className='home'>
+          <h1>Welcome to Conway's Game of life</h1>
+          <p className='text-style'>The Game of Life (an example of a cellular automaton ) is played on an infinite two-dimensional rectangular grid of cells. Each cell can be either alive or dead. The status of each cell changes each turn of the game (also called a generation) depending on the statuses of that cell's 8 neighbors.</p>
+          <h2>Rules</h2>
+          <ul>
+            <li className='text-style'>Any live cell with two or three live neighbours survives</li>
+            <li className='text-style'>Any dead cell with three live neighbours becomes a live cell.</li>
+            <li className='text-style'>All other live cells die in the next generation. Similarly, all other dead cells stay dead.</li>
+          </ul>
+          <button className='small-button' onClick={() => {setPage(!page)}}>Start Game</button>
         </div>
-        
-        {/* Grid speed */}
-        <label className='label-style'>Grid Speed:</label>
-        <div>        
-          <button className='small-button' onClick={() => setSpeed(100)}>Fast</button>
-          <button className='small-button' onClick={() => setSpeed(500)}>Normal</button>
-          <button className='small-button' onClick={() => setSpeed(1000)}>Slow</button>
-        </div>
-        
-        
-      </div>
-      
-    
-      <div
-      
-        style={{
-          display: "grid",
-          gridTemplateColumns: `repeat(${gridSize.numCols}, 20px)`
-        }}
-      >
-        {/*Generating the grid for the game*/}
-        {grid.map((rows, i) =>
-          rows.map((col, k) => (
-            <div
-              key={`${i}-${k}`}
-              onClick={() => {
-                if(!running) {
-                  const newGrid = produce(grid, gridCopy => {
-                    gridCopy[i][k] = grid[i][k] ? 0 : 1;
-                  })
-                  setGrid(newGrid);
-                };
-                
-              }}
-              className={grid[i][k] ? 'on' : 'off'}
-              style={{
-                width: 20,
-                height: 20,
-                border: "solid 1px white"
-              }}
-            />
-          ))
-        )}
-        {/* Counting the generations*/}
-        <h2>Generation: {generation}</h2>
-      </div>
-      
-    </div>
-  );
+        : 
+        <Grid />
+      }
+    </>
+  )
 };
 
 export default App;
